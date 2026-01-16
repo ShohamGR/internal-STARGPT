4c7809918f56e4fb9b3b1cbab8e2a3b550fb542b
ShohamGR <shohamg@campus.technion.ac.il>
2026-01-16 13:23:02 +0200

    New HF token

--- stargpt/constants.py
+++ stargpt/constants.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 WANDB_API_KEY = "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"
 WANDB_ENTITY = "tabular_data"
-HF_TOKEN = "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"
+HF_TOKEN = "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"
 os.environ["HF_TOKEN"] = HF_TOKEN
 KAGGLE_USERNAME = "shohamgrunblat"
 KAGGLE_KEY = "KGAT_5e15f00ca0d0a35ed695fbe6a84a5c70"
